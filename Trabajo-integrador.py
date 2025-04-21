6e72dc957cd46496bacf2c83d3bc2bf410d04415
Matias <matiasgiraldo1995@gmail.com>
2025-04-21 14:13:25 -0300

    variable respuesta_correcta cambiada (Parte Suma)

--- Trabajo-integrador.py
+++ Trabajo-integrador.py
@@ -40,7 +40,7 @@ print("Puede avanzar al siguiente nivel.")
 print("Sume los números que obtuvo de cada adivinanza e ingrese el resultado en sistema decimal.")
 suma = input("Ingrese su respuesta: ")
 # Suma de los numeros adivinados, xxxx porque desconozco los valores binarios (4 bits)
-respuesta_correcta = int("xxxx", 2) + int("xxxx", 2) + int("xxxx", 2) #cambiar las xxxx
+suma_correcta = int("0110", 2) + int("xxxx", 2) + int("xxxx", 2) #cambiar las xxxx
 # Cuando usamos int (valor, 2) estamos queriendo decir que el valor está en la base 2 y al poner int lo pasa a entero (guardo fuente)
 # Bucle que permite intentos al usuario en caso de haber escrito mal la respuesta de la suma
 correcto = False
@@ -49,7 +49,7 @@ while not correcto:
         suma = int(suma)
         if suma < 0:
             print("Error, ha ingresado un número negativo, vuelva a intentarlo")
-        elif suma != respuesta_correcta:
+        elif suma != suma_correcta:
             print("Incorrecto, vuelva a intentarlo")
         else:
             print("¡Correcto!")
